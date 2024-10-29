66764504129365ef97d6b1521203a2b452155469
Chinedu Ekene Okpala <allstackdev@gmail.com>
2024-10-29 09:20:29 +0000

    feat(Conference-page-ui-completed): shows details of a conference, list talks and attendees, delete talks

---
+++ src/hooks/useAlert.ts
@@ -0,0 +1,37 @@
+import { toast } from "react-toastify";
+import { useEffect, useRef } from "react";
+
+import { useAuthStore, useConferenceStore } from "src/stores";
+
+export const useAlert = (store: "auth" | "conference") => {
+  const stores = {
+    auth: useAuthStore,
+    conference: useConferenceStore,
+  };
+  const hasToastBeenCalled = useRef(false);
+  const { status, message, clearFormState } = stores[store].getState();
+
+  useEffect(() => {
+    if (message) {
+      if (!status && !hasToastBeenCalled.current) {
+        toast.error(message);
+        hasToastBeenCalled.current = true; // Set flag to true after first call
+      } else if (status && !hasToastBeenCalled.current) {
+        toast.success(message);
+        hasToastBeenCalled.current = true; // Set flag to true after first call
+      }
+    } else {
+      hasToastBeenCalled.current = false; // Reset flag if there's no message
+    }
+
+    const timer = setTimeout(clearFormState, 5000);
+
+    return () => {
+      clearFormState()
+      clearTimeout(timer)
+    }; // Cleanup timeout on unmount
+  }, [status, message, clearFormState]); // Add status and message to dependency array
+
+  return null;
+};
+
